0f5eb8b3eb64d90a284f451061444a96bc620b01
Linda TJ <sameer.tuteja05@gmail.com>
2023-06-24 12:31:53 +0200

    🦟

--- ros2_package/src/seek_thermal_ros/launch/thermal_publisher_launch.launch.py
+++ ros2_package/src/seek_thermal_ros/launch/thermal_publisher_launch.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
             ],
             remappings=[
                 ('in', '/thermalImage'),
-                ('out','/thermalImage/compressed')
+                ('out/compressed','/thermalImage/compressed')
             ]
         ),           
     ])
