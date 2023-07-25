5deec8b3d734b098a57a099b0c405baf3d43d02f
Shubham <52372631+skpawar1305@users.noreply.github.com>
2023-07-25 14:10:01 +0200

    fixup in thermal_publisher_launch.py
    adding None isn't allowed

--- seek_thermal_ros/launch/thermal_publisher_launch.py
+++ seek_thermal_ros/launch/thermal_publisher_launch.py
@@ -10,6 +10,8 @@ def generate_launch_description():
     rotationValue = LaunchConfiguration("rotationValue", default=270)
     greyscale = LaunchConfiguration("greyscale", default=False)
     
+    launch_list = []
+    
     seek_node = Node(
         package="seek_thermal_ros",
         executable="thermal_publisher",
@@ -21,6 +23,7 @@ def generate_launch_description():
         ],
         respawn=True,  # Automatically respawn the node if it exits
     )
+    launch_list.append(seek_node)
     
     compressed_seek_node = Node(
         package="image_transport",
@@ -35,6 +38,7 @@ def generate_launch_description():
             ("out/compressed","/thermalImage/compressed")
         ]
     )
+    launch_list.append(compressed_seek_node)
 
     # Check if ffmpeg plugin is installed
     if "ffmpeg_image_transport" in get_packages_with_prefixes():
@@ -51,11 +55,6 @@ def generate_launch_description():
                 ("out/ffmpeg","/thermalImage/ffmpeg")
             ]
         )
-    else:
-        ffmpeg_seek_node = None
+        launch_list.append(ffmpeg_seek_node)
 
-    return LaunchDescription([
-        seek_node,
-        compressed_seek_node,
-        ffmpeg_seek_node,
-    ])
+    return LaunchDescription(launch_list)
